b2e468ddc245c0c7e2533dc5b7d9fc52c9dd6c08
Glenn Adriel Mendonca <75622417+glenn169@users.noreply.github.com>
2026-08-03 12:22:47 +0100

    feat: add guided Packet Tracer labs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from 'react-router-dom'
 import { AppShell } from './components/AppShell'
 import { Dashboard } from './pages/Dashboard'
-import { Labs, NotFound, Progress } from './pages/SimplePages'
+import { NotFound, Progress } from './pages/SimplePages'
+import { LabPage, Labs } from './pages/LabPages'
 import { Practice, PracticeModulePage, QuizPage } from './pages/PracticePages'
 import { Navigate } from 'react-router-dom'
 
-export default function App() { return <AppShell><Routes><Route path="/" element={<Dashboard/>}/><Route path="/learn/*" element={<Navigate to="/practice" replace/>}/><Route path="/labs" element={<Labs/>}/><Route path="/practice" element={<Practice/>}/><Route path="/practice/:moduleId" element={<PracticeModulePage/>}/><Route path="/practice/:moduleId/:topicId" element={<QuizPage/>}/><Route path="/progress" element={<Progress/>}/><Route path="*" element={<NotFound/>}/></Routes></AppShell> }
+export default function App() { return <AppShell><Routes><Route path="/" element={<Dashboard/>}/><Route path="/learn/*" element={<Navigate to="/practice" replace/>}/><Route path="/labs" element={<Labs/>}/><Route path="/labs/:labId" element={<LabPage/>}/><Route path="/practice" element={<Practice/>}/><Route path="/practice/:moduleId" element={<PracticeModulePage/>}/><Route path="/practice/:moduleId/:topicId" element={<QuizPage/>}/><Route path="/progress" element={<Progress/>}/><Route path="*" element={<NotFound/>}/></Routes></AppShell> }
